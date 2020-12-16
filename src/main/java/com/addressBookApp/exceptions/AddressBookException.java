6a303de24ef0dfa43442ca3a4b8e01356846ff8c
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-12-17 00:26:03 +0530

    Added code to handle ID not found exception

---
+++ src/main/java/com/addressBookApp/exceptions/AddressBookException.java
@@ -0,0 +1,7 @@
+package com.addressBookApp.exceptions;
+
+public class AddressBookException extends RuntimeException{
+	public AddressBookException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/addressBookApp/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/addressBookApp/exceptions/AddressBookExceptionHandler.java
@@ -25,4 +25,12 @@ public class AddressBookExceptionHandler {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 				}
 
+	
+	@ExceptionHandler(AddressBookException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+			AddressBookException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",
+													exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
 }
